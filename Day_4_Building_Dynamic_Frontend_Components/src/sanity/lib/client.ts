ae2aa8d9361abacdb7a6b0661c9f86ac52b68f9f
SidraRaza <160983959+SidraRaza@users.noreply.github.com>
2025-01-22 09:52:41 +0500

    update

--- Day_4_Building_Dynamic_Frontend_Components/src/sanity/lib/client.ts
+++ Day_4_Building_Dynamic_Frontend_Components/src/sanity/lib/client.ts
@@ -3,7 +3,7 @@ import { createClient } from 'next-sanity';
 export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'qqxycy13',
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
-  apiVersion: '2025-10-01', // Stable API version
+  apiVersion: '2025-01-10', // Stable API version
   useCdn: true, // For fresh data, use false
 });
 
